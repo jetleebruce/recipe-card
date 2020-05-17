e17d59742eaa83f4bf810948c5dc959866512412
Serge Modin <modinsv@yandex.ru>
2020-05-17 22:54:04 +0300

    useState & useEffect for fetching API
    - useState & useEffect helps fetch API
    - Displaying in RandomMeal component the string from the array which have been fetched from the API

--- src/components/RandomMeal.js
+++ src/components/RandomMeal.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const API_URL = "https://www.themealdb.com/api/json/v1/1/random.php";
 
 const RandomMeal = () => {
-  return <div>Random</div>;
+  const [meal, setMeal] = useState(undefined);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((res) => {
+        setMeal(res.meals[0]);
+      });
+  }, []);
+
+  if (!meal) return null;
+
+  return (
+    <div>
+      <h1 style={{ height: "18px" }}>{meal.strMeal}</h1>
+    </div>
+  );
 };
 
 export default RandomMeal;
